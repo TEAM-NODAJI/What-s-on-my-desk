ebd5de9ea1df8699c2e8a90d1dceb43a56d83a16
minji.jo <mwinterj@daum.net>
2022-11-29 16:22:36 +0900

    :wrench:[change] 실시간 시계 기능 구현

---
+++ assets/script/clock.js
@@ -0,0 +1,26 @@
+const tabletDate = document.querySelector(".date-tablet");
+const tabletClock = document.querySelector(".clock-tablet");
+
+function getTime() {
+  const date = new Date();
+  const nowYear = date.getFullYear();
+  const nowMonth = date.getMonth() + 1;
+  const nowDate = date.getDate();
+
+  const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const nowDay = week[date.getDay()];
+
+  tabletDate.textContent = `${nowYear}년 ${nowMonth}월 ${nowDate}일 ${nowDay}`;
+  const nowHour = date.getHours();
+  const nowMinute = date.getMinutes();
+  const nowSecond = date.getSeconds();
+
+  console.log(nowHour, nowMinute, nowSecond)
+  tabletClock.textContent = `${nowHour} : ${nowMinute} : ${nowSecond}`;
+  requestAnimationFrame(getTime);
+}
+
+function liveClock() {
+  requestAnimationFrame(getTime);
+}
+liveClock()
