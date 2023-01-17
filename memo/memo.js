2596140bb1f6766d158bfa2019bc092ebfeb4645
minji.jo <mwinterj@daum.net>
2023-01-17 23:46:19 +0900

    :wrench:[change] 메모장 로컬스토리지 저장 및 렌더 구현

--- memo/memo.js
+++ memo/memo.js
@@ -1,17 +1,39 @@
+window.onload = loadMemo;
+
 const $inpMemo = document.querySelector(".inp-memo");
 const $btnMemo = document.querySelector(".btn-memo");
 const $memoWrap = document.querySelector(".wrap");
 
-const postMemo = () => {
+// 로컬스토리지에 값이 있다면 가져오기
+function loadMemo() {
+  if (localStorage.getItem("my-memo") == null) return;
+  let memoList = Array.from(JSON.parse(localStorage.getItem("my-memo")));
+
+  memoList.forEach((memo) => {
+    const memoCont = document.createElement("div");
+    const memoText = document.createElement("p");
+    const deleteBtn = document.createElement("button");
+    memoCont.classList.add("memo-cont");
+    $memoWrap.append(memoCont);
+    memoText.textContent = memo.memo;
+    deleteBtn.classList.add("delete-btn");
+    memoCont.append(memoText, deleteBtn);
+
+    // 입력 값의 길이에 따라 다른 style 지정
+    if (memo.memo.length < 30) {
+      memoText.classList.add("short-memo");
+    } else if (memo.memo.length > 107) {
+      memoText.classList.add("long-memo");
+    } else {
+      memoText.classList.add("regular-memo");
+    }
+  });
+}
+
+const renderMemo = () => {
   const getText = $inpMemo.value;
 
-  // 입력 값이 빈 문자열 일 경우
-  if (!getText) {
-    alert("메모를 입력해주세요!📝");
-    return;
-  }
-
-  // DOM 생성
+  //DOM 생성
   const memoCont = document.createElement("div");
   const memoText = document.createElement("p");
   const deleteBtn = document.createElement("button");
@@ -20,6 +42,7 @@ const postMemo = () => {
   memoText.textContent = getText;
   deleteBtn.classList.add("delete-btn");
   memoCont.append(memoText, deleteBtn);
+
   $inpMemo.value = "";
 
   // 입력 값의 길이에 따라 다른 style 지정
@@ -30,11 +53,31 @@ const postMemo = () => {
   } else {
     memoText.classList.add("regular-memo");
   }
-
-  // 메모 삭제
-  deleteBtn.addEventListener("click", () => {
-    $memoWrap.removeChild(memoCont);
-  });
 };
 
-$btnMemo.addEventListener("click", postMemo);
+const addMemo = () => {
+  const getText = $inpMemo.value;
+
+  // 입력 값이 유효하지 않을 경우
+  if (!getText) {
+    alert("메모를 입력해주세요!📝");
+    return;
+  }
+  localStorage.setItem(
+    "my-memo",
+    JSON.stringify([
+      ...JSON.parse(localStorage.getItem("my-memo") || "[]"),
+      { memo: getText },
+    ])
+  );
+
+  renderMemo();
+};
+
+const removeMemo = (event) => {
+  let memoList = Array.from(JSON.parse(localStorage.getItem("my-memo")));
+  // console.log(memoList);
+  // console.log(event.parentNode);
+};
+
+$btnMemo.addEventListener("click", addMemo);
